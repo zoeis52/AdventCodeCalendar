3adda29635d521bef3ccf6dd01184fbdc763429c
Zöe Steine-Hanson <zoe@Zoes-MacBook-Pro.local>
2020-12-01 14:47:07 -0800

    Solution to part 2

--- 2020_calendar/Day1/find_expenses.py
+++ 2020_calendar/Day1/find_expenses.py
@@ -1,6 +1,7 @@
 import sys
 
 def read_input():
+    "reads in the command line file and converts to a list of ints"
     fileName = sys.argv[1]
 
     lines = []
@@ -11,12 +12,33 @@ def read_input():
     return lines
 
 def find_2020_nums(expenses, add_search):
+    """
+    finds the 2 numbers in expenses that
+    sum to add_search
+    inputs: list of expenses as ints
+        and the number searching for
+    """
     for num_1 in expenses:
          less_than = add_search - num_1
          for num_2 in expenses:
+             # try to reduce to computations that could possibily be add_search
              if (num_2 <= less_than):
                  if num_1 + num_2 == add_search:
                      return (num_1, num_2)
+
+# terrible run time, is there a better solution?
+def find_three_2020_nums(expenses, add_search):
+    """
+    finds the 3 numbers in expenses that
+    sum to add_search
+    inputs: list of expenses as ints
+        and the number searching for
+    """
+    for num_1 in expenses:
+         for num_2 in expenses:
+             for num_3 in expenses:
+                 if num_1 + num_2 + num_3 == add_search:
+                     return (num_1, num_2, num_3)
 
 
 def main ():
@@ -25,6 +47,10 @@ def main ():
     expense_report.sort()
     num_1, num_2 = find_2020_nums(expense_report, add_search)
     print(num_1 * num_2)
+
+# for part 2
+    num_1, num_2, num_3 = find_three_2020_nums(expense_report, add_search)
+    print(num_1 * num_2 * num_3)
 
 
 main()
