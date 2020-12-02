536b04dda91048671bb49f2a1da796d127cb063a
Zöe Steine-Hanson <zoe@Zoes-MacBook-Pro.local>
2020-12-02 11:33:33 -0800

    Part 2 day 2 done

--- 2020_calendar/Day2/passwords.py
+++ 2020_calendar/Day2/passwords.py
@@ -13,6 +13,10 @@ def read_input():
     return lines
 
 def interpret_rules(rule_password_pair):
+    """
+    parses the given rule password pair for the
+    numbers and the letter for the rule
+    """
     first_num = 0
     second_num = 0
     letter = ''
@@ -25,7 +29,12 @@ def interpret_rules(rule_password_pair):
 
     return (int(first_num), int(second_num), letter)
 
-def find_valid_passwords(rule_password_pairs):
+def find_valid_sled_passwords(rule_password_pairs):
+    """
+    determines if passwords are valid based on the rule that the
+    password contains the given letter some number of times
+    (based on the first and second numbers in rule)
+    """
     valid_passwords = 0
     for pair in rule_password_pairs:
         first_num, second_num, letter = interpret_rules(pair)
@@ -35,10 +44,27 @@ def find_valid_passwords(rule_password_pairs):
 
     return valid_passwords
 
+def find_valid_tobaggan_passwords(rule_password_pairs):
+    """
+    determines if passwords are valid based on the rule that the
+    password contains the given letter at either the first provided
+    index or the second, but not both
+    """
+    valid_passwords = 0
+    for pair in rule_password_pairs:
+        first_num, second_num, letter = interpret_rules(pair)
+        password = pair[1]
+        if (password[first_num] == letter) != (password[second_num] == letter):
+            valid_passwords += 1
+
+    return valid_passwords
+
 
 def main ():
+    # Find the valid passwords
     rule_password_pairs = read_input()
-    print(find_valid_passwords(rule_password_pairs))
+    print(find_valid_sled_passwords(rule_password_pairs))
+    print(find_valid_tobaggan_passwords(rule_password_pairs))
 
 
 main()
